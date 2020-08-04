13e7c319009a3f1252587c160095a1f6d55e45c6
Julia Hunter <Julia.Hunter@ijyi.com>
2020-08-04 11:21:33 +0100

    Added .gitignore and forced it to stop tracking bin/ and obj/ by using git rm

--- charting-webapi/Controllers/OccupancyController.cs
+++ charting-webapi/Controllers/OccupancyController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Cosmos;
 
 namespace charting_webapi.Controllers
 {
@@ -20,7 +21,7 @@ namespace charting_webapi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Occupancy> Get()
+        public async Task<IEnumerable<Occupancy>> Get()
         {
             // var rng = new Random();
             // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -29,11 +30,40 @@ namespace charting_webapi.Controllers
             //     TemperatureC = rng.Next(-20, 55),
             //     Summary = Summaries[rng.Next(Summaries.Length)]
             // })
-            var occs = new List<Occupancy> {
-                new Occupancy  { GymId = "000005", NumberOfPeople = 3},
-                new Occupancy { GymId = "000004", NumberOfPeople = 10 },
-                new Occupancy { GymId = "000005", NumberOfPeople = 8 }};
-            return occs.ToArray();
+            return await GetFromStorage();
+
+            // var occs = new List<Occupancy> {
+            //     new Occupancy  { GymId = "000005", NumberOfPeople = 3},
+            //     new Occupancy { GymId = "000004", NumberOfPeople = 10 },
+            //     new Occupancy { GymId = "000005", NumberOfPeople = 8 }};
+            // return occs.ToArray();
+        }
+
+        private async Task<IEnumerable<Occupancy>> GetFromStorage()
+        {
+            // Initialize a new instance of CosmosClient using the built-in account endpoint and key parameters
+            var endpoint = "https://charting-stor.documents.azure.com:443/";
+            var key = "6rcotecXI2m7VpruVbL0F9di9kcpa169LLHZ27sycWkAJuPBsHwnndIOzYA6PbMPeYoWUCi7alGLopcmgVxMuQ==";
+            var databaseName = "gymoccupancydb";
+            var containerName = "hourlyoccupancy";
+
+            var sqlQueryText = "SELECT * FROM o WHERE o.gymid='000001'";
+            CosmosClient cosmosClient = new CosmosClient(endpoint, key);
+            var container = cosmosClient.GetContainer(databaseName, containerName);
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            var occupancies = new List<Occupancy>();
+            var test = container.GetItemQueryIterator<Occupancy>(queryDefinition);
+
+            var queryResultSetIterator = container.GetItemQueryIterator<Occupancy>(queryDefinition);
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Occupancy> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (Occupancy occ in currentResultSet)
+                {
+                    occupancies.Add(occ);
+                }
+            }
+            return occupancies;
         }
     }
 }
--- charting-webapi/Occupancy.cs
+++ charting-webapi/Occupancy.cs
@@ -1,15 +1,15 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace charting_webapi
 {
     public class Occupancy
     {
+        [JsonPropertyName("gymid")]
         public string GymId { get; set; }
         public int NumberOfPeople { get; set; }
 
         public DateTime Date { get; set; }
-
-
 
     }
 }
