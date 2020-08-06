a0427779d1b1af278560cd70b686a2dcc2d869bc
drjhunter <Julia.Hunter@ijyi.com>
2020-08-06 13:41:57 +0100

    Function with simple trigger and out binding tested in portal

--- charting-functions/OccupancyDataProcessorFn.cs
+++ charting-functions/OccupancyDataProcessorFn.cs
@@ -9,9 +9,15 @@ namespace GymOccupancy.Function
     public static class OccupancyDataProcessorFn
     {
         [FunctionName("OccupancyDataProcessorFn")]
-        public static void Run([BlobTrigger("rawoccupancydata/{name}", Connection = "gymoccupancystor_STORAGE")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("rawoccupancydata/{name}", Connection = "gymoccupancystor_STORAGE")] Stream myBlob, string name, out object cosmosDocument, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            cosmosDocument = new
+            {
+                gymid = "000005",
+                date = "2020-08-06T14:45:30",
+                numberofpeople = 18
+            };
         }
     }
 }
